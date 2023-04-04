0d63fb3dff0a94eac0bf177abae01deb0fbd3d54
mufatech <fabosedesidiqat@gmail.com>
2023-04-04 04:41:57 -0700

    Task 4 done

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 
 """ 
@@ -45,15 +46,27 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    
     def area(self):
         """returns the area of the rectangle"""
         return self.__width * self.__height
 
+    
     def perimeter(self):
         """returns the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width * 2) + (self.__height *2)
+    
+
+    def __str__(self):
+        """returns printable string representation of the rectangle"""
+        string = ""
+        if self.__width != 0 and self.__height != 0:
+            string += "\n".join("#" * self.__width for i in range(self.__height))
+            return string
 
 
-
+    def __repr__(self):
+        """returns a string representation of the Rectangle for recreate"""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.height)
