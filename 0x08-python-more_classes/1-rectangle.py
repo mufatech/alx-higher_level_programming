2de8578200861b2436bbcf691f3c67ac7095ebf2
mufatech <fabosedesidiqat@gmail.com>
2023-04-04 02:01:12 -0700

    task done

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,10 +12,12 @@ class Rectangle:
     
     @property
     def width(self):
+        """getter for private instance attribute width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """setter for private instance attribute width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -25,10 +27,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """getter for private instance attribute height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """setter for private instance attribute height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
