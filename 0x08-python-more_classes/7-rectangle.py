973d63f3ea596bf0fe7e03b0a3e5145ff0c3ca59
mufatech <fabosedesidiqat@gmail.com>
2023-04-04 07:04:28 -0700

    Task 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -7,19 +7,16 @@ Defines a class Rectangle
 class Rectangle:
     """Representation of a rectangle"""
 
-    number_of_instances = 0
-
     def __init__(self, width=0, height=0):
         """Initializes the rectangle"""
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
+    
 
     def __del__(self):
         """prints a string when an instance has been deleted"""
         print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
-
+        
     @property
     def width(self):
         """getter for the private instance attribute width"""
