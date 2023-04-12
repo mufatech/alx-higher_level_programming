291361410dabeca15bb378210785ac92278cd1e3
mufatech <fabosedesidiqat@gmail.com>
2023-04-11 19:38:20 -0700

    Python Inheritance

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Defines the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Represent Base Geometry"""
+
+    """A class with public instance methods area and integer_validator"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates that value is an integer and greater than 0"""
+        if type(value) != int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
