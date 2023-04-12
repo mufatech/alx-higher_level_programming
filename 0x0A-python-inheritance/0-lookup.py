d5980ab9cf595d52365a2ab2a7b8d208acc887db
mufatech <fabosedesidiqat@gmail.com>
2023-04-11 17:24:30 -0700

    Python Inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Consist of the lookup function
+"""
+
+
+def lookup(obj):
+    """returns a list of available attributes and methods of an object"""
+    return list(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,2 @@
+0x0A-python-inheritance
+This file consists of daily tasks on python inheritance of the ALX Software Engineering Program 
