74fec97e0ea29aff977810e1878fa935e5a04e62
mufatech <fabosedesidiqat@gmail.com>
2023-04-12 07:16:18 -0700

    Python Input and Output

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,5 +8,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Object to a text file, using a JSON representation"""
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, 'k', encoding='utf-8') as f:
         json.dump(my_obj, f)
